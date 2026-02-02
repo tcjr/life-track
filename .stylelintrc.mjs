bd36836f1dd291e6403136c28d51746fbf6e18ad
Tommy Carter <tommyjr@gmail.com>
2026-02-02 10:49:44 -0600

    Added tailwind + daisy

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,3 +1,17 @@
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['plugin'], // used by tailwind/daisy
+      },
+    ],
+    'import-notation': null, // allows tailwindcss import
+    'at-rule-empty-line-before': null,
+    'custom-property-empty-line-before': null,
+
+    // The DaisyUI themes use numbers, so "0" instead of "0deg"
+    'hue-degree-notation': 'number',
+  },
 };
--- app/templates/application.gts
+++ app/templates/application.gts
@@ -2,7 +2,7 @@ import { pageTitle } from 'ember-page-title';
 
 <template>
   {{pageTitle "LifeTrack"}}
-  <h2 id="title">Welcome to Ember</h2>
+  <h2 class="bg-primary text-primary-content">Welcome to Ember</h2>
 
   {{outlet}}
 </template>
