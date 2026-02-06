9e0caeca4e1798abf1040fc130dce06153f3b5e2
Tom Carter <tommyjr@gmail.com>
2026-02-06 04:04:33 +0000

    Removed problematic code

--- app/app.ts
+++ app/app.ts
@@ -2,14 +2,8 @@ import Application from '@ember/application';
 import compatModules from '@embroider/virtual/compat-modules';
 import Resolver from 'ember-resolver';
 import config from 'life-track/config/environment';
-//import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 import './styles/app.css';
-
-// This was causing problems in CI environments
-// if (macroCondition(isDevelopingApp())) {
-//   importSync('./deprecation-workflow');
-// }
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
